1193165c6bd065b576d903dd559aff9ec0868df2
Ali Aldabagh <alikais2005@gmail.com>
2022-11-24 22:53:31 -0500

    Complete qualified assignment, ...

---
+++ public/src/books.js
@@ -0,0 +1,37 @@
+function findAuthorById(authors, id) {
+  const found = authors.find((author) => author.id === id)
+  return found;
+}
+
+////done////
+
+function findBookById(books, id) {
+  const found = books.find((book) => book.id === id)
+
+  return found;
+
+}
+////done////
+
+function partitionBooksByBorrowedStatus(books) {
+  const resultOne = books.filter((book) => book.borrows[0].returned === false);
+  const resultTwo = books.filter((book) => book.borrows[0].returned === true);
+  return [resultOne, resultTwo];
+}
+
+////done////
+
+function getBorrowersForBook(book, accounts) {
+  return book.borrows.map(borrow => {
+    const account = accounts.find(account => account.id === borrow.id)
+    account.returned = borrow.returned
+    return account
+  }).slice(0, 10)
+}
+
+module.exports = {
+  findAuthorById,
+  findBookById,
+  partitionBooksByBorrowedStatus,
+  getBorrowersForBook,
+};
